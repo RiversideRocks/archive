44cb3593eb2c2baf31826bdee1d7aaf99052ce0d
Riverside Rocks <trent@riverside.rocks>
2020-12-18 20:36:33 -0500

    woot woot

---
+++ web/header.php
@@ -0,0 +1,10 @@
+<?php echo "<!-- Render time @" . time() . " -->"; ?>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/siimple@3.3.1/dist/siimple.min.css">
+<div class="siimple-navbar">
+    <a class="siimple-navbar-title">Riverside Rocks Archives</a>
+    <div class="siimple--float-right">
+        <a class="siimple-navbar-item" href="/snapshots">Snapshots</a>
+        <a class="siimple-navbar-item" href="/content">Content</a>
+    </div>
+</div>
+<center>
---
+++ web/home.php
@@ -0,0 +1,9 @@
+<?php
+
+include "header.php";
+?>
+
+<div class="siimple-jumbotron">
+    <div class="siimple-jumbotron-title">Welcome to the Riverside Rocks Archives!</div>
+    <div class="siimple-jumbotron-detail">This is the place to find old/outdated content kept on the internet for reference or historical value.</div>
+</div>
